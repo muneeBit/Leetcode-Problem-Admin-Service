955bc5a96d60ccc4557ff4dacd50425da9162010
Muneeb <muneebit010@gmail.com>
2026-05-05 09:29:21 +0000

    Feat: Added unimplemented error for api contracts

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,33 +1,45 @@
 const res = require("express/lib/response")
 
 function pingController(req,res) {
-    res.json({
+    return res.json({
         message: "problem controller is up"
     })
 }
 
 
 function addProblem(req,res) {
-
-
+    return res.status(501).json({
+        message: "Not Implemented"
+    })
 }
 
 function getProblem(req,res) {
-
+    return res.status(501).json({
+        message: "Not Implemented"
+    })
 
 }
 
 function getProblems(req,res) {
+    return res.status(501).json({
+        message: "Not Implemented"
+    })
 
 
 }
 
 function deleteProblem(req,res) {
+    return res.status(501).json({
+        message: "Not Implemented"
+    })
 
 
 }
 
 function updateProblem(req,res) {
+    return res.status(501).json({
+        message: "Not Implemented"
+    })
 
 
 }
